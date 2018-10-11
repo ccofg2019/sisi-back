0ebb3a4befb210457162f53391b561578b18e742
bruunofeliipe <brunofelipe.elias@hotmail.com>
2018-10-11 14:23:49 -0300

    01.01 feature/bck-0005 Alteração do seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(OccurrenceTypeTableSeeder::class);
+        $this->call(ZonesTableSeeder::class);
     }
 }
