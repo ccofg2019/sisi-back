fa460b4bd680ad56baa8714cae76b8ed4ab68348
“ThiagoALOliveira” <thiago.oliveira7870@gmail.com>
2018-10-12 19:25:45 -0300

    01.01 - feature/bck-0008 - Adicioanar involved_person and occurrence_objects

--- app/Services/OccurrenceReportService.php
+++ app/Services/OccurrenceReportService.php
@@ -13,7 +13,9 @@ use App\Services\Traits\CrudMethods;
 class OccurrenceReportService extends AppService
 {
     use CrudMethods {
-        all as protected processAll;
+        all    as protected processAll;
+        create as protected processCreate;
+
     }
 
     /**
@@ -21,14 +23,22 @@ class OccurrenceReportService extends AppService
      */
     protected $repository;
 
+    protected $involvedPeopleService;
+
+    protected $occurrenceObjectService;
+
     /**
      * RoleService constructor.
      *
      * @param OccurrenceReportRepository $repository
      */
-    public function __construct(OccurrenceReportRepository $repository)
+    public function __construct(OccurrenceReportRepository $repository,
+                                InvolvedPeopleService $involvedPeopleService,
+                                OccurrenceObjectService $occurrenceObjectService)
     {
         $this->repository = $repository;
+        $this->involvedPeopleService = $involvedPeopleService;
+        $this->occurrenceObjectService = $occurrenceObjectService;
     }
 
     /**
@@ -45,4 +55,39 @@ class OccurrenceReportService extends AppService
 
         return $this->processAll($limit);
     }
+
+    public function create(array $data)    {
+
+        $occurrence_report = $this->processCreate($data);
+
+        if(isset($occurrence_report)) {
+            if (isset($data['involved_person'])) {
+                foreach ($data['involved_person'] as $involved_people) {
+                    $person[] = $this->involvedPeopleService->create(
+                        array_merge($involved_people, ['occurrence_report_id' => $occurrence_report['data']['id']]));
+                }
+            }
+
+            if (isset($data['occurrence_objects'])) {
+                foreach ($data['occurrence_objects'] as $occurrence_object) {
+                    $this->OccurrenceObjectService->create(
+                        array_merge($occurrence_object, ['occurrence_report_id' => $occurrence_report['data']['id']])
+                    );
+                }
+            }
+
+            return [
+                "error" => "false",
+                "message" => "Ocorrêcia registrada com sucesso."
+            ];
+        } else {
+            return [
+                "error"     => "true",
+                "message"   => "Não foi possível registrar ocorrência."
+            ];
+        }
+
+
+    }
+
 }
