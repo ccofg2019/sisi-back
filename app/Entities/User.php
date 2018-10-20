d33dc255274665f063336c018c42a165099a0984
“ThiagoALOliveira” <thiago.oliveira7870@gmail.com>
2018-10-20 12:37:55 -0300

    02.01 - feature/bck-0011 - Criação da entidade logs e seus requisitos

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -72,4 +72,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function logs()
+    {
+        return $this->morphMany('App\Entities\Logs', 'loggable');
+    }
 }
